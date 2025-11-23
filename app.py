3f7840dfd63336518648b6fc3822a552c60d748b
TheCaveMembership <110085855+TheCaveMembership@users.noreply.github.com>
2025-11-23 03:44:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from staking import bp_stake
 from nft_api import bp_nft
 from izza_airdrop import izza_airdrop_bp
 from izza_bot import izza_bot_bp
-import bot_markets  # optional helpers for market scanning
+from bot_markets import bot_markets_bp
 from bot_trader import *  # optional depending on how you integrate
 
 from creatures_api import bp_creatures
@@ -160,6 +160,7 @@ app.register_blueprint(bp_stake)
 app.register_blueprint(bp_nft)
 app.register_blueprint(izza_airdrop_bp)
 app.register_blueprint(izza_bot_bp)
+app.register_blueprint(bot_markets_bp)
 app.register_blueprint(bp_creatures) 
 app.register_blueprint(friends_bp)
 
